7be8f35e74b9e9bcfb7bbafa865e5d058ada965d
YFanha <yannfnha@gmail.com>
2020-09-28 21:29:05 +0200

    css and js
    Gen: Change the nav's background image
    
    CSS: add a new style when the button is active and changed the title style
    
    JavaScript: add the function 'removeHidden' to add the class 'hidden' to the three parameters.

--- Voyage/js/script.js
+++ Voyage/js/script.js
@@ -4,16 +4,38 @@ document.addEventListener('DOMContentLoaded', function() {
     const btnHebergement = document.getElementById('btnHebergement');
     const btnActivites = document.getElementById('btnActivites');
 
+    const divParticipants = document.getElementById('divParticipants');
+    const divTransports = document.getElementById('divTransports');
+    const divHebergement =  document.getElementById('divHebergement');
+    const divActivites = document.getElementById('divActivites');
+
+    //afficher le menu des participants au chargement de la page
+    divParticipants.classList.remove('hidden');
+
     btnParticipants.addEventListener('click', function (){
-        document.getElementById('divParticipants').classList.remove('hidden');
+        divParticipants.classList.remove('hidden');
+        removeHidden(divTransports, divHebergement, divActivites);
     });
+
     btnTransports.addEventListener('click', function (){
-        document.getElementById('divTransports').classList.remove('hidden');
+        divTransports.classList.remove('hidden');
+        removeHidden(divParticipants, divHebergement, divActivites);
     });
+
     btnHebergement.addEventListener('click', function (){
-        document.getElementById('divHebergement').classList.remove('hidden');
+        divHebergement.classList.remove('hidden');
+        removeHidden(divParticipants, divTransports, divActivites);
     });
+
     btnActivites.addEventListener('click', function (){
-        document.getElementById('divActivites').classList.remove('hidden');
+        divActivites.classList.remove('hidden');
+        removeHidden(divParticipants, divTransports, divHebergement);
     });
-})
+
+    //add the class to hide the element
+    function removeHidden(divOne, divTwo, divThree){
+        divOne.classList.add('hidden');
+        divTwo.classList.add('hidden');
+        divThree.classList.add('hidden');
+    }
+});
